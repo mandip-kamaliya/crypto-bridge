062013ff3987e03f3cb37a13cbcbd81e31d3ade5
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-11-07 17:37:20 +0530

    FIX:ci/cd pipeline

--- src/MyTokenBridge.sol
+++ src/MyTokenBridge.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 import {OApp} from "@layerzerolabs/lz-oapp/contracts/OApp.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-contract MyTokenBridge{
 
-}
+contract MyTokenBridge {}
