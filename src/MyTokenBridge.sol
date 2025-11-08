c051583f29559ba863a610d444f1d81399d9e167
mandip kamaliya <mandipkamaliya7@gmail.com>
2025-11-08 17:19:38 +0530

    added state variables

--- src/MyTokenBridge.sol
+++ src/MyTokenBridge.sol
@@ -4,4 +4,7 @@ pragma solidity ^0.8.20;
 import {OApp} from "@layerzerolabs/lz-oapp/contracts/OApp.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract MyTokenBridge {}
+contract MyTokenBridge is OApp{
+    IERC20 public immutable originalToken;
+    uint32 public destinationEid;
+}
